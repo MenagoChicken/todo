b7b71258bf173bcc498192eb10680b8304b28143
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-08-19 23:39:05 +0100

    todoList

--- app.js
+++ app.js
@@ -6,7 +6,25 @@ const userName = localStorage.getItem("username") || "";
 
 /***** logic *****/
 
+/*** usser name -> localc storage ***/
 nameInput.value = userName;
 nameInput.addEventListener("change", (name) => {
   localStorage.setItem("username", name.target.value);
+});
+
+console.log(newTodoForm);
+
+/*** todolist from and to local storage ***/
+newTodoForm.addEventListener("submit", (list) => {
+  list.preventDefault();
+
+  const todo = {
+    content: list.target.elements.content.value,
+    category: list.target.elements.category.value,
+    done: false,
+    creationTime: new Date().getTime(),
+  };
+
+  list.push(todo);
+  localStorage.setItem("todoList", JSON.stringify(todo));
 });
