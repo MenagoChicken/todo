0f02773af7b7514f8a5ad53c2fad9809a73dfb74
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-08-19 22:48:39 +0100

    constants

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log("hello World!");
+const todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+const nameInput = document.querySelector("#name");
+const newTodoForm = document.querySelector("#new-todo-form");
+const userName = localStorage.getItem("username") || "";
