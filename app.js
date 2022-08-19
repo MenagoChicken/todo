fb3469c86509405509ce3c3b1b5c4f3281a478d5
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-08-19 22:58:22 +0100

    username

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
+/***** contstants *****/
 const todoList = JSON.parse(localStorage.getItem("todoList")) || [];
 const nameInput = document.querySelector("#name");
 const newTodoForm = document.querySelector("#new-todo-form");
 const userName = localStorage.getItem("username") || "";
+
+/***** logic *****/
+
+nameInput.value = userName;
+nameInput.addEventListener("change", (name) => {
+  localStorage.setItem("username", name.target.value);
+});
