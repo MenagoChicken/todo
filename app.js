89f297275be5ed86f2949fb63be75add5a1361d7
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-08-20 14:08:16 +0100

    adding to local storage - solved

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /***** contstants *****/
-const todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+const todoList = JSON.parse(localStorage.getItem("localTodoList")) || [];
+const todoListHTML = document.querySelector("#todo-list");
 const nameInput = document.querySelector("#name");
 const newTodoForm = document.querySelector("#new-todo-form");
 const userName = localStorage.getItem("username") || "";
@@ -12,19 +13,22 @@ nameInput.addEventListener("change", (name) => {
   localStorage.setItem("username", name.target.value);
 });
 
-console.log(newTodoForm);
-
 /*** todolist from and to local storage ***/
-newTodoForm.addEventListener("submit", (list) => {
-  list.preventDefault();
+newTodoForm.addEventListener("submit", (item) => {
+  item.preventDefault();
 
   const todo = {
-    content: list.target.elements.content.value,
-    category: list.target.elements.category.value,
+    content: item.target.elements.content.value,
+    category: item.target.elements.category.value,
     done: false,
     creationTime: new Date().getTime(),
   };
 
-  list.push(todo);
-  localStorage.setItem("todoList", JSON.stringify(todo));
+  console.log("BEFORE ADDING ELEMENT " + todoList);
+  todoList.push(todo);
+  localStorage.setItem("localTodoList", JSON.stringify(todoList));
+  console.log("AFTER ADDING ELEMENT " + todoList);
+  item.target.reset();
+
+  displayTodoList();
 });
