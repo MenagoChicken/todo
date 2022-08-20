b7e56c51d632da6195dd2d12e635dc30c493d243
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-08-20 14:44:00 +0100

    errorMessage

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const todoListHTML = document.querySelector("#todo-list");
 const nameInput = document.querySelector("#name");
 const newTodoForm = document.querySelector("#new-todo-form");
 const userName = localStorage.getItem("username") || "";
+const emptyErrorMessage = document.querySelector("#empty-error-message");
 
 /***** logic *****/
 
@@ -17,18 +18,59 @@ nameInput.addEventListener("change", (name) => {
 newTodoForm.addEventListener("submit", (item) => {
   item.preventDefault();
 
-  const todo = {
-    content: item.target.elements.content.value,
-    category: item.target.elements.category.value,
-    done: false,
-    creationTime: new Date().getTime(),
-  };
+  console.log("content " + item.target.elements.content.value.type);
+  console.log("content " + item.target.elements.category.value.type);
 
-  console.log("BEFORE ADDING ELEMENT " + todoList);
-  todoList.push(todo);
-  localStorage.setItem("localTodoList", JSON.stringify(todoList));
-  console.log("AFTER ADDING ELEMENT " + todoList);
-  item.target.reset();
+  if (
+    !item.target.elements.content.value ||
+    !item.target.elements.category.value
+  ) {
+    emptyErrorMessage.style.visibility = "visible";
+    console.log(todoList);
+  } else {
+    emptyErrorMessage.style.visibility = "hidden";
+    const todo = {
+      content: item.target.elements.content.value,
+      category: item.target.elements.category.value,
+      done: false,
+      creationTime: new Date().getTime(),
+    };
+
+    console.log("BEFORE ADDING ELEMENT " + todoList);
+    todoList.push(todo);
+    localStorage.setItem("localTodoList", JSON.stringify(todoList));
+    console.log("AFTER ADDING ELEMENT " + todoList);
+    item.target.reset();
+  }
 
   displayTodoList();
 });
+
+function displayTodoList() {
+  todoListHTML.innerHTML = "";
+
+  todoList.forEach((element) => {
+    //   <div class="todo-item">
+    //   <label>
+    //     <input type="checkbox" />
+    //     <span class="bubble business"></span>
+    //   </label>
+    //   <div class="todo-content">
+    //     <input type="text" value="Create todo app." readonly />
+    //   </div>
+    //   <div class="actions">
+    //     <button class="edit">Edit</button>
+    //     <button class="delete">Delete</button>
+    //   </div>
+    // </div>
+    const divTodoItem = document.createElement("div");
+    divTodoItem.classList.add("todo-item");
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+    const span = document.createElement("span");
+    const content = document.createElement("div");
+    const actions = document.createElement("div");
+    const editBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button");
+  });
+}
